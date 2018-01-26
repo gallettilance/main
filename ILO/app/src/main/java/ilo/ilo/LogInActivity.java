0aafe5f79a5151dd4857e67e7bf08249f67f452e
galletti94 <galletti@bu.edu>
2018-01-26 11:38:16 -0500

    Updating: Adding base project and main activities

---
+++ ILO/app/src/main/java/ilo/ilo/LogInActivity.java
@@ -0,0 +1,13 @@
+package ilo.ilo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
