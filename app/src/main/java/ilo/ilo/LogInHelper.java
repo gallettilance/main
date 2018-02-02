1161a4e4f930d448abd6b51db8fec2071d2e36e6
Emmanuel Amponsah <emmanuela956@gmail.com>
2018-02-02 15:49:38 -0500

    Starting skelton for SignUp, need db to finish

---
+++ app/src/main/java/ilo/ilo/LogInHelper.java
@@ -0,0 +1,35 @@
+package ilo.ilo;
+
+
+public class LogInHelper {
+
+    private String mEmail;
+    private String mUserName;
+    private String mPassword;
+
+    public LogInHelper(String email, String user, String pass){
+        this.mEmail = email;
+        this.mPassword = pass;
+        this.mUserName = user;
+    }
+    private boolean isValidEmail(){
+        //check for valid email address
+        return mEmail.contains("@") && mEmail.length() > 8;
+    }
+
+
+    private boolean isValidUserName(){
+        // simple check to make sure username is valid
+        return mUserName.length()  > 5;
+    }
+
+    private  boolean isValidPassword(){
+        // simple check for password length
+        return mPassword.length() > 8;
+    }
+
+    public  boolean validateForm(){
+        return isValidEmail() && isValidPassword() && isValidUserName();
+    }
+
+}
--- app/src/main/java/ilo/ilo/SignUpActivity.java
+++ app/src/main/java/ilo/ilo/SignUpActivity.java
@@ -1,13 +1,30 @@
 package ilo.ilo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    LogInHelper mForm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        findViewById(R.id.button_signup_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //populate form with info from layout
+                mForm = new LogInHelper("test@wow.com", "first", "notsafe");
+                if(mForm.validateForm()){
+                    //TODO Make database call with proper user information, get back authkey and load Viewprofile
+                    Intent i = new Intent(SignUpActivity.this, ViewProfileActivity.class);
+                    startActivity(i);
+                }
+               else{
+                    //TODO Highlight parts of form that aren't working and or user already exist
+                }
+            }
+        });
     }
 }
