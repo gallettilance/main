097c39907c0bbf1330bdbf1bdacf18b61dc9fbf4
Robin Rhodes <rhodesr@bu.edu>
2018-04-10 19:25:06 -0700

    Changes to profile view

--- app/src/main/java/ilo/ilo/ViewProfileActivity.java
+++ app/src/main/java/ilo/ilo/ViewProfileActivity.java
@@ -2,12 +2,35 @@ package ilo.ilo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class ViewProfileActivity extends AppCompatActivity {
+
+// onClicks, isEditable(boolean) corresponding with onclick
+    TextView mFullname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_profile);
+        setContentView(R.layout.activity_profile_view);
+
+        mFullname = findViewById(R.id.text_full_name);
+        mFullname.setText("Joe Smith");
+
+        findViewById(R.id.button_view_edit_icon).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent editProfileIntent = new Intent(ViewProfileActivity.this, EditProfileActivity.class);
+                startActivity(editProfileIntent);
+                //              }
+            }
+        });
+
+
+
     }
 }
